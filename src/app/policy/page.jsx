498e154fdc0853ab36677428a44f500cd783088b
suman yadav <suman@imagemediaworld.com>
2024-01-24 19:20:34 +0530

    Initial commit

---
+++ src/app/policy/page.jsx
@@ -0,0 +1,209 @@
+import React from "react";
+
+
+const Page = () => {
+    return (
+      <div>
+        <h1>Privacy Policy</h1>
+        <p>Last updated January 02, 2024</p>
+        <p>
+          This privacy notice for IMW THE AGENCY WORLDWIDE PRIVATE LIMITED (doing
+          business as Image Media World) ('we', 'us', or 'our'), describes how and
+          why we might collect, store, use, and/or share ('process') your
+          information when you use our services ('Services'), such as when you:
+        </p>
+        <ul>
+          <li>Download and use our mobile application (My Vote Verdict), or any other application of ours that links to this privacy notice</li>
+          <li>Engage with us in other related ways, including any sales, marketing, or events</li>
+        </ul>
+        <p>
+          Questions or concerns? Reading this privacy notice will help you
+          understand your privacy rights and choices. If you do not agree with
+          our policies and practices, please do not use our Services. If you
+          still have any questions or concerns, please contact us at
+          info@myvoteverdict.com.
+        </p>
+  
+        <section>
+          <h2>Summary of Key Points</h2>
+          <p>
+            This summary provides key points from our privacy notice, but you can
+            find out more details about any of these topics by clicking the link
+            following each key point or by using our table of contents below to
+            find the section you are looking for.
+          </p>
+          <ul>
+            <li>What personal information do we process? When you visit, use, or navigate our Services, we may process personal information depending on how you interact with us and the Services, the choices you make, and the products and features you use. Learn more about personal information you disclose to us.</li>
+            <li>Do we process any sensitive personal information? We do not process sensitive personal information.</li>
+            <li>Do we receive any information from third parties? We may receive information from public databases, marketing partners, social media platforms, and other outside sources. Learn more about information collected from other sources.</li>
+          </ul>
+        </section>
+  
+        <section>
+          <h2>Table of Contents</h2>
+          <ol>
+            <li>WHAT INFORMATION DO WE COLLECT?</li>
+            <li>HOW DO WE PROCESS YOUR INFORMATION?</li>
+            <li>WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?</li>
+            <li>WHAT IS OUR STANCE ON THIRD-PARTY WEBSITES?</li>
+            <li>DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</li>
+            <li>HOW DO WE HANDLE YOUR SOCIAL LOGINS?</li>
+            <li>HOW LONG DO WE KEEP YOUR INFORMATION?</li>
+            <li>HOW DO WE KEEP YOUR INFORMATION SAFE?</li>
+            <li>WHAT ARE YOUR PRIVACY RIGHTS?</li>
+            <li>CONTROLS FOR DO-NOT-TRACK FEATURES</li>
+            <li>DO WE MAKE UPDATES TO THIS NOTICE?</li>
+            <li>HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</li>
+            <li>HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</li>
+          </ol>
+        </section>
+  
+        <section>
+          <h2>1. WHAT INFORMATION DO WE COLLECT?</h2>
+          <p>
+            <strong>Personal information you disclose to us</strong>
+          </p>
+          <p>In Short: We collect personal information that you provide to us.</p>
+          <p>
+            We collect personal information that you voluntarily provide to us
+            when you register on the Services, express an interest in obtaining
+            information about us or our products and Services, when you
+            participate in activities on the Services, or otherwise when you
+            contact us.
+          </p>
+          <p>
+            <strong>Option 2</strong>
+          </p>
+          <p>
+            We only gather info you willingly share: during registration,
+            expressing interest, participating in activities, or when you reach
+            out to us. Your choice, your data
+          </p>
+          <p>
+            Personal Information Provided by You. The personal information that we
+            collect depends on the context of your interactions with us and the
+            Services, the choices you make, and the products and features you use.
+            The personal information we collect may include the following:
+          </p>
+          <ul>
+            <li>phone numbers</li>
+            <li>email addresses</li>
+            <li>usernames</li>
+            <li>passwords</li>
+            <li>contact or authentication data</li>
+          </ul>
+          <p>
+            We collect personal information that you voluntarily provide to us
+            when you register on the Services, express an interest in obtaining
+            information about us or our products and Services, when you
+            participate in activities on the Services, or otherwise when you
+            contact us.
+          </p>
+        </section>
+  
+        <h1>Terms and Conditions</h1>
+  
+        <p>
+          Welcome to My Vote Verdict! These terms and conditions outline the rules
+          and regulations for the use of My Vote Verdict's Website, located at
+          https://myvoteverdict.com.
+        </p>
+        <p>
+          {" "}
+          By accessing this website, we assume you accept these terms and
+          conditions. Do not continue to use My Vote Verdict if you do not agree
+          to take all of the terms and conditions stated on this page.
+        </p>
+  
+        <p>
+          The following terminology applies to these Terms and Conditions, Privacy
+          Statement and Disclaimer Notice and all Agreements: "Client", "You" and
+          "Your" refers to you, the person log on this website and compliant to
+          the Company's terms and conditions. "The Company", "Ourselves", "We",
+          "Our" and "Us", refers to our Company. "Party", "Parties", or "Us",
+          refers to both the Client and ourselves. All terms refer to the offer,
+          acceptance and consideration of payment necessary to undertake the
+          process of our assistance to the Client in the most appropriate manner
+          for the express purpose of meeting the Client's needs in respect of
+          provision of the Company's stated services, in accordance with and
+          subject to, prevailing law of in. Any use of the above terminology or
+          other words in the singular, plural, capitalization and/or he/she or
+          they, are taken as interchangeable and therefore as referring to same.
+        </p>
+        <h1>Cookies</h1>
+        <p>
+          We employ the use of cookies. By accessing My Vote Verdict, you agreed
+          to use cookies in agreement with the My Vote Verdict's Privacy Policy.
+          Most interactive websites use cookies to let us retrieve the user's
+          details for each visit. Cookies are used by our website to enable the
+          functionality of certain areas to make it easier for people visiting our
+          website. Some of our affiliate/advertising partners may also use
+          cookies.
+        </p>
+  
+        <h1>License</h1>
+        <p>
+          Unless otherwise stated, My Vote Verdict and/or its licensors own the
+          intellectual property rights for all material on My Vote Verdict. All
+          intellectual property rights are reserved. You may access this from My
+          Vote Verdict for your own personal use subjected to restrictions set in
+          these terms and conditions.
+        </p>
+        <p>You must not:</p>
+        <ul>
+          <li>Republish material from My Vote Verdict</li>
+          <li>Sell, rent, or sub-license material from My Vote Verdict</li>
+          <li>Reproduce, duplicate, or copy material from My Vote Verdict</li>
+          <li>Redistribute content from My Vote Verdict</li>
+        </ul>
+        <p>
+          This Agreement shall begin on the date hereof. Our Terms and Conditions
+          were created with the help of the Free Terms and Conditions Generator.
+        </p>
+        <p>
+          Parts of this website offer an opportunity for users to post and
+          exchange opinions and information in certain areas of the website. My
+          Vote Verdict does not filter, edit, publish or review Comments prior to
+          their presence on the website. Comments do not reflect the views and
+          opinions of My Vote Verdict, its agents and/or affiliates. Comments
+          reflect the views and opinions of the person who posts their views and
+          opinions. To the extent permitted by applicable laws, My Vote Verdict
+          shall not be liable for the Comments or for any liability, damages or
+          expenses caused and/or suffered as a result of any use of and/or posting
+          of and/or appearance of the Comments on this website.
+        </p>
+        <p>
+          My Vote Verdict reserves the right to monitor all Comments and to remove
+          any Comments which can be considered inappropriate, offensive or causes
+          breach of these Terms and Conditions.
+        </p>
+        <p>You warrant and represent that:</p>
+        <ul>
+          <li>
+            You are entitled to post the Comments on our website and have all
+            necessary licenses and consents to do so;
+          </li>
+          <li>
+            The Comments do not invade any intellectual property right, including
+            without limitation copyright, patent, or trademark of any third party;
+          </li>
+          <li>
+            The Comments do not contain any defamatory, libelous, offensive,
+            indecent, or otherwise unlawful material which is an invasion of
+            privacy;
+          </li>
+          <li>
+            The Comments will not be used to solicit or promote business or custom
+            or present commercial activities or unlawful activity.
+          </li>
+        </ul>
+        <p>
+          You hereby grant My Vote Verdict a non-exclusive license to use,
+          reproduce, edit and authorize others to use, reproduce and edit any of
+          your Comments in any and all forms, formats or media.
+        </p>
+      </div>
+    );
+  };
+
+export default Page;
